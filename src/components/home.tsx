2e0dc9332217a1e0f827621951889536778e9778
mariano.martinez <mariano.martinez>
2021-12-07 15:00:47 -0600

    stack navigation added

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,18 +1,18 @@
 import * as React from 'react';
-/* import { HomeScreen, LoginScreen } from '../screens';
+import { HomeScreen, LoginScreen } from '../screens';
 import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native'; */
+import { NavigationContainer } from '@react-navigation/native';
 
-/* const Stack = createStackNavigator(); */
+const Stack = createStackNavigator();
 
 export function Home() {
   return (
-    <></>
-  );
-  {/* <NavigationContainer>
+    <NavigationContainer>
       <Stack.Navigator initialRouteName="Root">
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Auth" component={LoginScreen} />
       </Stack.Navigator>
-    </NavigationContainer> */}
+    </NavigationContainer>
+  );
+  
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
-/* import { Home } from './components/home'; */
+/* import { Text } from 'react-native'; */
+import { Home } from './components/home';
 
 export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
 
 export function Iron() {
-  /* return <Home />; */
-  return <></>
+  return <Home />;
+  /* return <Text>Hola</Text> */
 
 }
